5aa7ffbba0a3524f6e0357d5452f67a7ce5d29ca
Settarov Ali <ali.settarov.dev@gmail.com>
2021-12-02 13:48:04 +0200

    Change server/index.js path

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 }
 
