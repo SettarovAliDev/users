6f66c799eb7bf5bdf1c7aebda86719703f205e2b
Settarov Ali <ali.settarov.dev@gmail.com>
2021-12-03 12:15:39 +0200

    Change index.js

--- server/index.js
+++ server/index.js
@@ -14,9 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-  });
 }
 
 // ROUTES
