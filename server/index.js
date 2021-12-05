5d8eb9d1e0322ba82fc06813dd0e69f1a013ada4
Settarov Ali <ali.settarov.dev@gmail.com>
2021-12-05 11:24:26 +0200

    Revert "Change proxy"
    This reverts commit 1bc84a31c450d3d54543578e98e92b63a75a0cbd.

--- server/index.js
+++ server/index.js
@@ -3,18 +3,19 @@ const cors = require("cors");
 const path = require("path");
 const bodyParser = require("body-parser");
 const pool = require("./db");
-const proxy = require("http-proxy-middleware");
+// const proxy = require("http-proxy-middleware");
 
 require("dotenv").config();
 
 const app = express();
+// const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 5000;
 
 // MIDDLEWARE
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(proxy(["/api"], { target: "http://localhost:5000" }));
+// app.use(proxy(["/api"], { target: "http://localhost:5000" }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
